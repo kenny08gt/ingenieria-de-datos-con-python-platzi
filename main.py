d936ac084a2550b9023d9598fcad9b5a4139a4ca
Alan Hurtarte <alan@mypostcard.com>
2021-10-11 19:47:47 -0600

    Saving news on csv file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import argparse
 import logging
 import re
+import datetime
+import csv
 
 from requests.exceptions import HTTPError
 from urllib3.exceptions import MaxRetryError
@@ -29,9 +31,23 @@ def _news_scraper(news_site_uid):
         if article:
             logger.info('Article fetched')
             articles.append(article)
-            print(article.title)
 
-    print(len(articles))
+    _save_articles(news_site_uid, articles)
+
+
+def _save_articles(news_site_uid, articles):
+    now = datetime.datetime.now().strftime('%Y_%m_%d')
+    out_file_name = '{news_site_uid}_{datetime}_articles.csv'.format(
+        news_site_uid=news_site_uid, datetime=now)
+    csv_headers = list(filter(
+        lambda property: not property.startswith('_'), dir(articles[0])))
+    with open(out_file_name, mode='w+') as f:
+        writer = csv.writer(f)
+        writer.writerow(csv_headers)
+
+        for article in articles:
+            row = [str(getattr(article, prop)) for prop in csv_headers]
+            writer.writerow(row)
 
 
 def _fetch_article(news_site_uid, host, link):
